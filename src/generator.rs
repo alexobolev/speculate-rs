927a372df5fee5bcfe3bc5b253ab5efe31356a85
Alex Sobolev <code.sobolev@gmail.com>
2022-11-28 00:00:21 +0200

    Brush up code of 'Generate' implementations.

--- src/generator.rs
+++ src/generator.rs
@@ -3,38 +3,35 @@ use proc_macro2::{Ident, TokenStream};
 use quote::{quote_spanned, ToTokens};
 
 pub trait Generate {
-    fn generate(self, up: Option<&Describe>) -> TokenStream;
+    fn generate(self, outer: Option<&Describe>) -> TokenStream;
 }
 
 impl Generate for Block {
-    fn generate(self, up: Option<&Describe>) -> TokenStream {
+    fn generate(self, outer: Option<&Describe>) -> TokenStream {
         match self {
-            Block::Describe(describe) => describe.generate(up),
-            Block::It(it) => it.generate(up),
+            Block::Describe(describe) => describe.generate(outer),
+            Block::It(it) => it.generate(outer),
             Block::Item(item) => item.into_token_stream(),
         }
     }
 }
 
 impl Generate for Describe {
-    fn generate(mut self, up: Option<&Describe>) -> TokenStream {
-        if let Some(ref up) = up {
-            self.before = up
-                .before
-                .iter()
+    fn generate(mut self, outer: Option<&Describe>) -> TokenStream {
+        if let Some(outer) = outer {
+            self.before = outer.before.iter()
                 .chain(self.before.iter())
-                .cloned()
-                .collect();
-            self.after = self.after.iter().chain(up.after.iter()).cloned().collect();
+                .cloned().collect();
+
+            self.after = self.after.iter()
+                .chain(outer.after.iter())
+                .cloned().collect();
         }
 
-        let items = self
-            .blocks
-            .iter()
+        let name = &self.name;
+        let items = self.blocks.iter()
             .map(|block| block.clone().generate(Some(&self)))
             .collect::<Vec<_>>();
-
-        let name = &self.name;
 
         quote_spanned!(name.span() =>
             mod #name {
@@ -48,21 +45,19 @@ impl Generate for Describe {
 }
 
 impl Generate for It {
-    fn generate(self, up: Option<&Describe>) -> TokenStream {
-        let blocks = if let Some(ref up) = up {
-            up.before
-                .iter()
-                .chain(Some(self.block).iter())
-                .chain(up.after.iter())
-                .cloned()
-                .collect()
-        } else {
-            vec![self.block]
+    fn generate(self, outer: Option<&Describe>) -> TokenStream {
+        let blocks = match outer {
+            Some(outer) => {
+                outer.before.iter()
+                    .chain(Some(self.block).iter())
+                    .chain(outer.after.iter())
+                    .cloned().collect()
+            },
+            None => vec![self.block],
         };
 
-        let stmts = blocks.into_iter().flat_map(|block| block.stmts);
-
         let name = Ident::new(&format!("test_{}", self.name), self.name.span());
+        let stmts = blocks.into_iter().flat_map(|block| block.stmts);
         let attributes = self.attributes;
 
         quote_spanned!(name.span() =>
